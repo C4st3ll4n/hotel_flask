fe917a9462ab59db3e4dc3e065347db4641f3922
Henrique Souza <henrique.souza@elostecnologia.com.br>
2020-11-18 11:44:43 -0300

    integração com o mailgun para confirmação de email

--- models/user.py
+++ models/user.py
@@ -1,4 +1,8 @@
+from flask import url_for, request
+from requests import post
+
 from sql_alchemy import db
+from utils.mailgun import *
 
 
 class UserModel(db.Model):
@@ -6,19 +10,25 @@ class UserModel(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    login = db.Column(db.String(40))
-    password = db.Column(db.String(40))
+    login = db.Column(db.String(40), nullable=False, unique=True)
+    password = db.Column(db.String(40), nullable=False)
+    status = db.Column(db.Boolean, default=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
 
-    def __init__(self, name, login, password):
+    def __init__(self, name, login, password, email, status=False):
         self.name = name
         self.login = login
         self.password = password
+        self.status = status
+        self.email = email
 
     def json(self):
         return {
             "user_id": self.user_id,
             "name": self.name,
             "login": self.login,
+            "email":self.email,
+            "status": self.status
         }
 
     @classmethod
@@ -31,6 +41,13 @@ class UserModel(db.Model):
     @classmethod
     def find_by_login(cls, login):
         user = cls.query.filter_by(login=login).first()
+        if user:
+            return user
+        return
+
+    @classmethod
+    def find_by_email(cls, email):
+        user = cls.query.filter_by(email=email).first()
         if user:
             return user
         return None
@@ -48,4 +65,18 @@ class UserModel(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-
+    def send_email_confirm(self):
+        base_url = request.url_root[:-1]
+        url = url_for('userconfirm', user_id=self.user_id)
+        link_confirmacao = str(base_url + url)
+        url_post = f"https://api.mailgun.net/v3/{mailgun_domain}/messages"
+        return post(
+            url_post,
+            auth=("api", mailgun_api_key),
+            data={"from": "NÃO RESPONDA <henrique.souza@elostecnologia.com.br>",
+                  "to": self.email,
+                  "subject": "Confirmar cadastro",
+                  "text": f"Clique no link, e confirme seu cadastro\n {link_confirmacao}",
+                  'html': confirm_template(link_confirmacao)
+                  },
+        )
--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,5 @@
+import traceback
+
 from flask_jwt_extended import create_access_token, jwt_required, get_raw_jwt
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
@@ -9,7 +11,8 @@ from models.user import UserModel
 class User(Resource):
     argumentos = reqparse.RequestParser()
     argumentos.add_argument("name", type=str, required=True, help="Name cannot be null")
-    argumentos.add_argument("login", type=float, required=True, help="Rating cannot be null")
+    argumentos.add_argument("login", type=str, required=True, help="Login cannot be null")
+    argumentos.add_argument("status", type=bool, required=False, help="Status cannot be null")
 
     def get(self, user_id):
         user = UserModel.find_user(user_id=user_id)
@@ -49,39 +52,48 @@ class User(Resource):
 
 class UserRegister(Resource):
     def post(self):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument("name", type=str, required=True, help="Name cannot be null")
-        argumentos.add_argument("login", type=str, required=True, help="Login cannot be null")
-        argumentos.add_argument("password", type=str, required=True, help="Password cannot be null")
-        dados = argumentos.parse_args()
+        args = reqparse.RequestParser()
+        args.add_argument("name", type=str, required=True, help="Name cannot be null")
+        args.add_argument("email", type=str, required=True, help="Email cannot be null")
+        args.add_argument("login", type=str, required=True, help="Login cannot be null")
+        args.add_argument("password", type=str, required=True, help="Password cannot be null")
 
-        if UserModel.find_by_login(dados['login']):
+        dados = args.parse_args()
+
+        if UserModel.find_by_login(dados['login']) or UserModel.find_by_email(dados['email']):
             return {"message": "User already exists"}
 
         user = UserModel(**dados)
-
+        user.status = False
         try:
             user.save_user()
+            user.send_email_confirm()
             return {"message": "User created sucessfully"}, 201
-        except:
+        except Exception as e:
+            user.delete_user()
+            traceback.print_exc()
             return {"message": "Failed to create a user"}, 500
 
 
 class UserLogin(Resource):
     def post(self):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument("login", type=str, required=True, help="Login cannot be null")
-        argumentos.add_argument("password", type=str, required=True, help="Password cannot be null")
-        dados = argumentos.parse_args()
+        args = reqparse.RequestParser()
+        args.add_argument("login", type=str, required=True, help="Login cannot be null")
+        args.add_argument("password", type=str, required=True, help="Password cannot be null")
+
+        dados = args.parse_args()
 
         try:
             user = UserModel.find_by_login(dados['login'])
             if user:
-                if safe_str_cmp(user.password, dados['password']):
-                    token_acesso = create_access_token(identity=user.user_id)
-                    return {"message": "Sucessfully logged", "access_token":token_acesso}, 200
+                if user.status:
+                    if safe_str_cmp(user.password, dados['password']):
+                        token_acesso = create_access_token(identity=user.user_id)
+                        return {"message": "Sucessfully logged", "access_token": token_acesso}, 200
+                    else:
+                        return {"message": "Password dont match"}, 403
                 else:
-                    return {"message": "Password dont match"}, 403
+                    return {"message": "User not confirmed"}, 403
 
         except Exception as e:
             return {"message": "Failed to return a user", "exception": e.__str__()}, 500
@@ -95,3 +107,16 @@ class UserLogout(Resource):
         jwt_id = get_raw_jwt()['jti']
         DENIALLIST.add(jwt_id)
         return {"message": "Logged out sucessfully"}, 200
+
+
+class UserConfirm(Resource):
+
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_user(user_id)
+        if not user:
+            return {"message": "User does not exists"}, 401
+
+        user.status = True
+        user.save_user()
+        return {"message": "Confirmed user"}, 200
